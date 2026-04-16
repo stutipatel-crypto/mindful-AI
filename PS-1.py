9c7c39130742b68a85ed7d969d3912eb27d9805b
stutipatel-crypto <stuti.patel@avantika.edu.in>
2026-04-16 17:00:48 +0530

    ps-1

--- PS-1.py
+++ PS-1.py
@@ -1,2 +1,13 @@
-for i in range(1,6):
+#N = int(input("Enter N: "))
+
+#for i in range(1, N+1):
+    #print(i)
+
+#N = int(input("Enter N: "))
+#for i in range(N, 0, -1):
+    #print(i)
+
+N = int(input("Enter N: "))
+
+for i in range(1, N+1, 2):
     print(i)
