b5a9fcccb26b36879035565e947ababf48094737
stutipatel-crypto <stuti.patel@avantika.edu.in>
2026-04-17 00:42:08 +0530

    ps-13

---
+++ PS-13.py
@@ -0,0 +1,16 @@
+#a
+#N = int(input("Enter N: "))
+
+#for i in range(1, N+1):
+    #for j in range(1, i+1):
+        #print(chr(64 + j), end='')
+    #print()
+    #N = int(input("Enter N: "))
+#b
+#N = int(input("Enter N: "))
+#for i in range(1, N+1):
+    #for j in range(i, 0, -1):
+        #print(chr(64 + j), end='')
+    #print()
+K = int(input("Enter K: "))
+print(chr(64 + K))
