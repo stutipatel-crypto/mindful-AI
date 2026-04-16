2f802726c7fd11e60d73e7623bb492d137f58421
stutipatel-crypto <stuti.patel@avantika.edu.in>
2026-04-16 23:47:47 +0530

    ps-11

---
+++ PS-11.py
@@ -0,0 +1,12 @@
+N = int(input("Enter N: "))
+
+for i in range(1, N+1):
+    # print spaces
+    for j in range( N - i):
+        print(' ', end='')
+    
+    # print stars
+    for j in range(1, 2*i):
+        print('*', end='')
+    
+    print()
