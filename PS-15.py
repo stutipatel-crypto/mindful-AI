a634b3cdb2c27ebb5d2d1f5a52f52cc05de3fbf7
stutipatel-crypto <stuti.patel@avantika.edu.in>
2026-04-17 12:17:39 +0530

    ps-15

---
+++ PS-15.py
@@ -0,0 +1,26 @@
+#bug A
+#N = int(input("Enter N: "))
+#i=1
+#while i <= N:
+    #print(i)
+    #i += 1
+#bub B
+#N = int(input("Enter N: "))
+#for i in range(1, N+1):          
+    #for j in range(1, i+1):      
+        #print('*', end='')
+    #print()
+#c
+N = int(input("Enter N: "))
+for i in range(1, N+1):
+    for j in range(1, i+1):   
+        print('*', end='')
+    print()
+
+#bugD
+#N = int(input("Enter N: "))
+
+#for i in range(1, N+1):
+    #for j in range(1, i+1):
+        #print('*', end='')
+    #print()
