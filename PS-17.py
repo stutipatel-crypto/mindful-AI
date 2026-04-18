f1e25c7009c9754a92ae441d00c46f367951e806
stutipatel-crypto <stuti.patel@avantika.edu.in>
2026-04-18 11:36:00 +0530

    ps-17

---
+++ PS-17.py
@@ -0,0 +1,25 @@
+#b
+#scores = [45, 72, 88, 60, 95]
+#for score in scores:
+    #if score >= 60:
+        #print("Score:", score, "pass")
+    #else:
+        #print("Score:", score, "fail")
+#c
+#scores = [45, 72, 88, 60, 95]
+counter = 0
+#for score in scores:
+    #if score >= 60:
+        #print("Score:", score, "pass")
+        #counter = counter + 1
+    #else:
+        #print("Score:", score, "fail")
+#print("Number of pass students:", counter)
+#d
+
+scores = [45, 72, 88, 60, 95]
+
+total=0
+for score in scores:
+    total = total + score
+print("total of all scores:", total)
--- PS-9.py
+++ PS-9.py
@@ -1,16 +1,18 @@
-#N = int(input("Enter N: "))
+#a
+N = int(input("Enter N: "))
 
-#for i in range(1,N+1):
-    #for j in range(1,i+1):
-        #print(j, end='')
-    #print()
+for i in range(1,N+1):
+    for j in range(1,i+1):
+        print(j, end='')
+    print()
+#b
+N = int(input("Enter N: "))
 
-#N = int(input("Enter N: "))
-
-#for i in range(1,N+1):
-    #for j in range(1,i+1):
-        #print(i, end='')
-    #print()
+for i in range(1,N+1):
+    for j in range(1,i+1):
+        print(i, end='')
+    print()
+   #c 
 N = int(input("Enter N: "))
 
 for i in range(1, N+1):
